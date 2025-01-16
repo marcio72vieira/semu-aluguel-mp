99b48a68345d0538bfb6dc72530b824060733b75
marcio72vieira <marcio72vieira@hotmail.com>
2025-01-16 18:07:56 -0300

    Aprimorando Filtro Checklist

--- app/Http/Controllers/Admin/ChecklistController.php
+++ app/Http/Controllers/Admin/ChecklistController.php
@@ -11,15 +11,17 @@ class ChecklistController extends Controller
 {
     public function index(Request $request)
     {
+
         // Recuperando somente os Requerente (com registros relacionados) que estão na situação de pendente(2) ou corrigidos(4), para análise do Servidor da SEMU
         //$requerentes = Requerente::with(['regional', 'municipio', 'tipounidade', 'unidadeatendimento', 'documentos', 'user'])->where('status', '=', 2)->orWhere('status', '=', 4)->orderBy('nomecompleto')->paginate(10);
-        
+
+        /*
         // Query original sem filtro
         $requerentes = Requerente::with(['regional', 'municipio', 'tipounidade', 'unidadeatendimento', 'documentos', 'user'])->orderBy('nomecompleto')->paginate(10);
         return view('admin.checklists.index', [ 'requerentes' => $requerentes ]);
+        */
 
-        
-        /* 
+
         // Query com filtro
         $requerentes = DB::table('requerentes')
         ->join('regionais', 'regionais.id', '=', 'requerentes.regional_id')
@@ -27,25 +29,41 @@ class ChecklistController extends Controller
         ->join('tipounidades', 'tipounidades.id', '=', 'requerentes.tipounidade_id')
         ->join('unidadesatendimentos', 'unidadesatendimentos.id', '=', 'requerentes.unidadeatendimento_id')
         ->join('users', 'users.id', '=', 'requerentes.user_id')
-        
-        ->select('requerentes.id', 'requerentes.nomecompleto AS nomecompletorequerente', 
+        //->join('documentos', 'documentos.requerente_id', '=', 'requerentes.id')
+
+
+        ->select('requerentes.id', 'requerentes.nomecompleto AS nomecompletorequerente', 'requerentes.foneresidencial', 'requerentes.fonecelular', 'requerentes.estatus',
             'regionais.nome AS nomeregional','municipios.nome AS nomemunicipio',
             'tipounidades.nome AS nometipounidade', 'unidadesatendimentos.nome AS nomeunidade',
             'users.nome AS nomeoperador')
-        
-        
-        ->when($request->has('nomerequerente'), function($query) use($request) {
-                $query->where('requerentes.nomecompleto', 'like', '%'. $request->nomerequerente . '%');
+
+
+        ->when($request->has('requerente'), function($query) use($request) {
+                $query->where('requerentes.nomecompleto', 'like', '%'. $request->requerente . '%');
         })
-        ->orderByDesc('id')
+        ->when($request->has('municipio'), function($query) use($request) {
+            $query->where('municipios.nome', 'like', '%'. $request->municipio . '%');
+        })
+        ->when($request->has('tipounidade'), function($query) use($request) {
+            $query->where('tipounidades.nome', 'like', '%'. $request->tipounidade . '%');
+        })
+        ->when($request->has('unidade'), function($query) use($request) {
+            $query->where('unidadesatendimentos.nome', 'like', '%'. $request->unidade . '%');
+        })
+
+        //->groupBy('documentos.requerente_id')
+
+
+        ->orderByDesc('nomecompletorequerente')
         ->paginate(10);
 
         return view('admin.checklists.index', [
-            'menu' => 'users',
+            'exibirfiltro' => 'nao',
             'requerentes' => $requerentes,
-            'nomerequerente' => $request->nomerequerente,
-        ]); 
-        */
+            'requerente' => $request->requerente,
+
+        ]);
+
 
     }
 }
--- resources/views/admin/checklists/index.blade.php
+++ resources/views/admin/checklists/index.blade.php
@@ -13,36 +13,54 @@
     <div class="mb-4 shadow card border-light">
         <div class="gap-2 card-header hstack">
             <span class="flex-row mt-1 mb-1 ms-auto d-sm-flex">
-                <label id="ocultarExibirPaineldeFiltragem" style="cursor: pointer; font-size: 17px;"><i id="iconeVisao" class="fas fa-eye" style=" margin-right: 5px;"></i>Exibir Filtro</label>
+                <label id="ocultarExibirPaineldeFiltragem" style="cursor: pointer; font-size: 17px;"><i id="iconeVisao" class="fa-solid fa-filter" style=" margin-right: 5px;"></i>Filtro</label>
                 {{-- <a href="{{ route('requerente.create') }}" class="btn btn-success btn-sm me-1"><i class="fa-solid fa-eye-slash"></i> Filtrar </a> --}}
                 {{-- <a href="{{ route('user.pdflistusers') }}" class="btn btn-danger btn-sm me-1" target="_blank"><i class="fa-solid fa-file-pdf"></i> pdf</a> --}}
             </span>
         </div>
-        
+
         {{-- inicio filtro --}}
-        <div class="mt-1 mb-4 shadow card border-light" id="formularioFiltragem" style="display:none;">
+        <div class="mt-1 mb-4 shadow card border-light" id="formularioFiltragem" style="display: {{ $exibirfiltro == 'sim' ? '' : 'none' }}">
             {{-- <div class="gap-2 card-header hstack"> <span>Filtro</span> </div> --}}
 
             <div class="card-body">
                 <form action="{{ route('checklist.index') }}">
                     <div class="row">
                         {{-- Colunas, quando for dispositivos médios(md) ocupe 4 grids e quando for dispositivos pequenos(sm) ocupe 12 grids--}}
-                        <div class="col-md-4 col-sm-12">
+                        <div class="col-md-2 col-sm-12">
                             <label class="form-label" for="name">Requerente</label>
-                            <input type="text" name="requerente" id="requerente" class="form-control" value="" placeholder="Nome da requerente">
+                            <input type="text" name="requerente" id="requerente" class="form-control" value="{{ $requerente }}" placeholder="Nome da requerente">
                         </div>
 
-                        <div class="col-md-4 col-sm-12">
-                            <label class="form-label" for="email">E-mail</label>
-                            <input type="text" name="email" id="email" class="form-control" value="" placeholder="E-mail do usuário">
+                        <div class="col-md-2 col-sm-12">
+                            <label class="form-label" for="municipio">Município</label>
+                            <input type="text" name="municipio" id="municipio" class="form-control" value="" placeholder="Município">
                         </div>
 
-                        <div class="col-md-4 col-sm-12">
-                            <label class="form-label" for="role">Papel</label>
-                            <input type="text" name="role" id="role" class="form-control" value="" placeholder="Papel do usuário">
+                        <div class="col-md-2 col-sm-12">
+                            <label class="form-label" for="role">Tipo</label>
+                            <input type="text" name="tipounidade" id="tipounidade" class="form-control" value="" placeholder="Tipo unidade">
+                        </div>
+
+                        <div class="col-md-2 col-sm-12">
+                            <label class="form-label" for="role">Unidade</label>
+                            <input type="text" name="unidade" id="unidade" class="form-control" value="" placeholder="Unidade de atendimento">
+                        </div>
+
+                        <div class="col-md-2 col-sm-12">
+                            <label class="form-label" for="role">Analista</label>
+                            <input type="text" name="analista" id="analista" class="form-control" value="" placeholder="Analista">
+                        </div>
+
+                        <div class="pt-3 col-md-2 col-sm-12">
+                            <div style="margin-left: 40%; margin-top:20px;">
+                                <button type="submit" name="pesquisar" class="btn btn-info btn-sm"><i class="fa-solid fa-magnifying-glass"></i> Pesquisar</button>
+                                <a href="{{ route('checklist.index')}}" class="btn btn-warning btn-sm"><i class="fa-solid fa-trash"></i> Limpar</a>
+                            </div>
                         </div>
                     </div>
 
+                    {{--
                     <div class="mb-3 row">
                         <div class="col-md-4 col-sm-12">
                             <label class="form-label" for="data_cadastro_inicio">Data cadastro início</label>
@@ -59,6 +77,8 @@
                             <a href="{{ route('user.index')}}" class="btn btn-warning btn-sm"><i class="fa-solid fa-trash"></i> Limpar</a>
                         </div>
                     </div>
+                    --}}
+
                 </form>
             </div>
         </div>
@@ -95,21 +115,20 @@
                     {{-- @dd("Acessando uma propriedade de um dos relacionamento de requerentes", $requerentes[0]['documentos'][0]['url']) --}}
                     {{-- @dd("Acessando uma propriedade de um dos relacionamento de requerentes", $requerentes[0]['documentos'][0]['user_id']) --}}
                     {{-- @dd("Acessando a propriedade nome do relacionamento regional de requernete", $requerentes[0]['regional']['nome']) --}}
-                    
-                   
+
+
 
                     @forelse ($requerentes as $requerente)
                         <tr>
+                            <td>{{ $requerente->id }}</td>
+                            <td>{{ $requerente->nomecompletorequerente }}</td>
+                            <td>{{ $requerente->nomemunicipio }}</td>
+                            <td>{{ $requerente->nometipounidade }}</td>
+                            <td>{{ $requerente->nomeunidade }}</td>
+                            <td>{{ $requerente->nomeoperador }}</td>
+                            <td>{{ $requerente->foneresidencial }} <br> {{ $requerente->fonecelular }}</td>
+
                             {{--
-                                COM FILTRO
-                                <td>{{ $requerente->id }}</td>
-                                <td>{{ $requerente->nomecompletorequerente }}</td>
-                                <td>{{ $requerente->nomemunicipio }}</td>
-                                <td>{{ $requerente->nometipounidade }}</td>
-                                <td>{{ $requerente->nomeunidade }}</td>
-                                <td>{{ $requerente->nomeoperador }}</td>
-                                <td>(98) 987002233 <br> (98) 32433212 $requerente->foneresidencial <br> --}} {{-- $requerente->fonecelular  </td> 
-                            --}}
                             <td>{{ $requerente->id }}</td>
                             <td>{{ $requerente->nomecompleto }}</td>
                             <td>{{ $requerente->municipio->nome }}</td>
@@ -117,30 +136,36 @@
                             <td>{{ $requerente->unidadeatendimento->nome }}</td>
                             <td>{{ $requerente->user->nome }}</td>
                             <td>{{ $requerente->foneresidencial }} <br> {{ $requerente->fonecelular }} </td>
-                            <td> 
+                            --}}
+
+                            <td>
                                 {{-- $requerente->servidorResponsavelPelaAnaliseDocumentos(3)[0]->nomecompleto --}}
                                 {{-- @foreach ($requerente->documentos as $documento) {{ $documento->user->nomecompleto }} @if ($loop->first) @break @endif @endforeach --}}
-                                
-                                @foreach ($requerente->documentos as $documento)
-                                    {{-- 
+
+                                {{-- @foreach ($requerente->documentos as $documento) --}}
+
+                                    {{--
                                         No momento do cadastro dos documentos o usuário(user_id) que será cadastrado na tabela "documentos", é o usuário(user_id)
                                         do Assistente Social(Operador), responsável pelo cadastro  da Requerente. Só quando  for feita a análise  dos documentos  é  que  o
                                         (user_id) do Servidor da Semu(Analista) irá  substituir o (user_id) do Assistente Social na tabela "documentos". Enquanto a análise
                                         não for concluída, irá aparecer  as "reticẽncias", indicando que os documentos  da Requerente ainda não foram analisados.
-                                        Seria um erro exibir o nome do Assistente Social(operador) como sendo o Nome do Servidor da SEMU(analista). 
-                                        Observação importante: 
+                                        Seria um erro exibir o nome do Assistente Social(operador) como sendo o Nome do Servidor da SEMU(analista).
+                                        Observação importante:
                                         Nos testes, quando o Administrador cadastrar uma requerente e realizar a análise dos documentos, seu nome não irá aparecer como
-                                        sendo o analista(coluna: "analisado por"), isto faz todo sentido, levando em consideração a condição abaixo, ou seja, se o nome  de 
+                                        sendo o analista(coluna: "analisado por"), isto faz todo sentido, levando em consideração a condição abaixo, ou seja, se o nome  de
                                         quem cadastrou a requerente é o mesmo nome de quem analisou os documentos deverá aparecer as reticências ... na coluna "analisado por"
                                         Esta lógica será aplicada se Uma servidora da SEMU assumir o papel de Operadora e Analista do mesmo Requerente.
-                                    --}} 
+                                    --}}
+                                    {{--
                                     @if ($documento->user->nome == $requerente->user->nome)
                                         <i class="fa-solid fa-ellipsis" title="documentos sendo anexados..."></i>
                                     @else
                                         {{ $documento->user->nome }}
                                     @endif
-                                    @if ($loop->first) @break @endif 
+                                    @if ($loop->first) @break @endif
                                 @endforeach
+                                --}}
+                                Nome do Analista
                             </td>
                             <td>
                                 @if($requerente->estatus == 1) <span style="font-size: 14px;"> <i class="fa-solid fa-shoe-prints"></i> em andamento </span> @endif  {{-- falta anexar todos os documentos --}}
@@ -181,12 +206,12 @@
     <script>
         // Esconde/Exibe os cards para ampliar área de visualização
         $("#ocultarExibirPaineldeFiltragem").click(function(){
-            if($(this).text() == "Ocultar Filtro"){
+            if($(this).text() == "Ocultar"){
                 //$(this).text("Exibir");
-                $("#ocultarExibirPaineldeFiltragem").html("<i id='iconeVisao' class='fas fa-eye' style='margin-right: 5px;'></i>Exibir Filtro");
+                $("#ocultarExibirPaineldeFiltragem").html("<i id='iconeVisao' class='fa-solid fa-filter' style='margin-right: 5px;'></i>Filtro");
             }else {
                 //$(this).text("Ocultar");
-                $("#ocultarExibirPaineldeFiltragem").html("<i id='iconeVisao' class='fas fa-eye-slash' style='margin-right: 5px;'></i>Ocultar Filtro");
+                $("#ocultarExibirPaineldeFiltragem").html("<i id='iconeVisao' class='fas fa-eye-slash' style='margin-right: 5px;'></i>Ocultar");
             }
 
             $("#formularioFiltragem").toggle();
